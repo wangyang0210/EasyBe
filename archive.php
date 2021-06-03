e333c0d0b74987d59c7d16873b6b677b6cddde2b
wangyangyang <wangyangyang@ecdpower.net>
2021-06-03 13:29:02 +0800

    fix-tag|category

--- archive.php
+++ archive.php
@@ -1,27 +1,45 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 
-    <div class="col-mb-12 col-8" id="main" role="main">
-        <h3 class="archive-title"><?php $this->archiveTitle(array(
-            'category'  =>  _t('分类 %s 下的文章'),
-            'search'    =>  _t('包含关键字 %s 的文章'),
-            'tag'       =>  _t('标签 %s 下的文章'),
-            'author'    =>  _t('%s 发布的文章')
-        ), '', ''); ?></h3>
+<div id="mainContent">
+    <div class="forFlow">
+        <h1 class="archive-title"><?php $this->archiveTitle(array(
+            'category'  =>  _t('文章分类 - %s'),
+            'search'    =>  _t('关键字 - %s'),
+            'tag'       =>  _t('标签 - %s '),
+            'author'    =>  _t('作者 - %s')
+        ), '', ''); ?></h1>
         <?php if ($this->have()): ?>
     	<?php while($this->next()): ?>
-            <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
-    			<h2 class="post-title" itemprop="name headline"><a itemprop="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-    			<ul class="post-meta">
-    				<li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
-    				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></li>
-    				<li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-                    <li itemprop="interactionCount"><a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
-    			</ul>
-                <div class="post-content" itemprop="articleBody">
-        			<?php $this->content('- 阅读剩余部分 -'); ?>
+                <div class="day">
+                    <div class="postTitle">
+                        <a id="homepage1_HomePageDays_DaysList_ctl00_DayList_TitleUrl_0" class="postTitle2"
+                           href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+                    </div>
+                    <div class="postCon">
+                        <div class="c_b_p_desc">
+                            <?php $this->excerpt('200', '...'); ?><a class="read-more"
+                                                                     href="<?php $this->permalink() ?>"
+                            <a href="<?php $this->permalink() ?>" class="c_b_p_desc_readmore">阅读全文</a>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                    <div class="postDesc">
+                        <img class="author-thumb" src="<?php $this->options->adminpicUrl(); ?>">
+                        <?php $this->author(); ?>
+                        <?php $this->date("Y-m-d H:i:s"); ?>
+                        阅读(<?php get_post_view($this) ?>)
+                        评论(<?php $this->commentsNum('%d'); ?>)
+                        <?php if ($this->user->hasLogin()) : ?>
+                            <a href="<?php $this->options->adminUrl(); ?>write-post.php?cid=<?php echo $this->cid; ?>"
+                               target="_blank">编辑</a>
+                        <?php endif; ?>
+                    </div>
+                    <div class="clear"></div>
+
+                    <div class="postSeparator"></div>
+
                 </div>
-    		</article>
     	<?php endwhile; ?>
         <?php else: ?>
             <article class="post">
@@ -30,7 +48,8 @@
         <?php endif; ?>
 
         <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;'); ?>
-    </div><!-- end #main -->
+    </div>
+</div>
 
 	<?php $this->need('sidebar.php'); ?>
 	<?php $this->need('footer.php'); ?>
