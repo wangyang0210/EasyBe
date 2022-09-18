02548a98fd8888191f104afb2d33dc848912fd19
wangyang <wangyang.0210@foxmail.com>
2022-09-19 00:03:35 +0800

    refactor: title

--- src/components/status/status.js
+++ src/components/status/status.js
@@ -9,16 +9,13 @@
 
 export default function main() {
     let status = {};
-
     // 提取url信息
     status.url = window.location.href;
     let tmp = status.url.split("/");
     status.user = tmp[3];
     status.articleId = '';
-
     // 主页链接
     status.homeUrl = [tmp[0], tmp[1], tmp[2], tmp[3]].join("/");
-
     // 判断当前页面类型
     if (!$('#topics').length) {
         status.pageType = 'home'; // 当前页面为主页
@@ -30,11 +27,9 @@ export default function main() {
         } else {
             status.pageType = 'article'; // 当前页面为文章页
         }
-
         // 提取文章id
         let endVal = (tmp[tmp.length - 1]).split(".");
         status.articleId = endVal[0];
     }
-
     return status;
 }
