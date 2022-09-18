fc05aef054dc5ba6ec2fa4241886880e285eef61
wangyang <wangyang.0210@foxmail.com>
2022-09-18 23:33:27 +0800

    refactor: commentjs

--- src/components/comment/comment.js
+++ src/components/comment/comment.js
@@ -6,7 +6,7 @@
  * ----------------------------------------------
  * @describe: 评论处理
  */
-import defaultAvatarImg from './../../images/webp/default_avatar.webp';
+import defaultAvatarImg from './../../images/default_avatar.webp';
 
 export default function main(_) {
     let setComment = () => {
@@ -21,23 +21,14 @@ export default function main(_) {
                     idInfo = commentBody.length ? commentBody.attr('id').split('_') : undefined;
 
                 if (idInfo && idInfo.length > 0) {
-                    let id = idInfo[idInfo.length - 1],
-                        idTmp = id.toString().match(/[0-9]/g);
-
+                    let id = idInfo[idInfo.length - 1], idTmp = id.toString().match(/\d/g);
                     if ($.isArray(idTmp)) id = idTmp.join('');
-
-                    let op = $('#comment_' + id + '_avatar'), patch  = op.length > 0 ? op.text().trim()
-                        : defaultAvatarImg;
-
+                    let op = $('#comment_' + id + '_avatar'), patch  = op.length > 0 ? op.text().trim() : defaultAvatarImg;
                     let ac = $('#a_comment_author_' + id), ah = ac.length ? ac.attr('href') : 'javascropt:void(0);';
-
                     avatarHtml = '<div class="feedbackAvatar"><a href="' + ah + '" target="_blank"><img src="'+patch+'"/></a></div>';
                     obj.prepend(avatarHtml);
                 }
-
-                if (feedbackListSubtitle.length && feedbackListSubtitle.find('.louzhu').length) {
-                    feedbackListSubtitle.addClass('feedbackListSubtitle-louzhu');
-                }
+                if (feedbackListSubtitle.length && feedbackListSubtitle.find('.louzhu').length) feedbackListSubtitle.addClass('feedbackListSubtitle-louzhu');
             });
             $(feedbackItem[0]).css('padding-top', '0');
             $(feedbackItem[feedbackItem.length - 1]).css('padding-bottom', '0');
@@ -50,9 +41,10 @@ export default function main(_) {
             _.__tools.clearIntervalTimeId(_.__timeIds.commentTId);
         }
     },1000);
+
     $(document).ajaxSuccess(function (event, xhr, settings) {
-        if (settings.url.includes("archive")) {
-            setComment();
-        }
+        if (settings.url.includes("archive")) setComment();
     });
 }
+
+
