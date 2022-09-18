4ae3f9890828102f703e0f047c7b7866b78592b2
wangyang <wangyang.0210@foxmail.com>
2022-09-18 23:52:42 +0800

    refactor: progress

--- src/components/progress/progress.js
+++ src/components/progress/progress.js
@@ -11,7 +11,6 @@ import ToProgress from "../../vendor/ToProgress/ToProgress";
 export default function main(_) {
     $('#blog-news').prepend('<div id="progressBar"></div>');
     let progressBar = ToProgress && new window.ToProgress(_.__config.progressBar, '#progressBar');
-
     // 添加事件监听
     _.__event.scroll.handle.push(() => {
         progressBar.setProgress(_.__tools.getScrollPercent());
