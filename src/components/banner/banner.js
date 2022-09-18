78966184a50b26d3c2865b4fe3d3be5ca9795eec
wangyang <wangyang.0210@foxmail.com>
2022-09-18 23:27:35 +0800

    refactor: bannerjs

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -7,8 +7,8 @@
  * @describe: banner 处理
  */
 import bannerTemp from '../../template/banner.html';
-import homeTopBg from "../../images/webp/home_top_bg.webp";
-import netHomeTopBg from "../../images/webp/nothome_top_bg.webp";
+import homeTopBg from "../../images/home_top_bg.webp";
+import netHomeTopBg from "../../images/nothome_top_bg.webp";
 
 export default function main(_) {
 
@@ -24,11 +24,9 @@ export default function main(_) {
 
         // 设置图片
         if (_.__status.pageType === 'home') {
-            topImg = _.__config.banner.home.background.length > 0
-                ? _.__config.banner.home.background : [homeTopBg];
+            topImg = _.__config.banner.home.background.length > 0 ? _.__config.banner.home.background : [homeTopBg];
         }  else {
-            topImg = _.__config.banner.article.background.length > 0
-                ? _.__config.banner.article.background : [netHomeTopBg];
+            topImg = _.__config.banner.article.background.length > 0 ? _.__config.banner.article.background : [netHomeTopBg];
             height = '40vh';
             $('#homeTopTitle').hide();
             $('.scroll-down').hide();
@@ -57,9 +55,9 @@ export default function main(_) {
             });
         } else {
             // 随机指定一个图片
-            topImg.length > 0 ?
-                (topImg.length > 1 ? bgImg = topImg[_.__tools.randomNum(0, topImg.length - 1)] : bgImg = topImg[0])
-                : bgImg = "";
+
+            if (topImg.length > 1) bgImg = topImg[_.__tools.randomNum(0, topImg.length - 1)]
+            else bgImg = topImg[0] || ""
 
             mainHeader.css({
                 'background': '#222 url(\'' + encodeURI(bgImg) + '\')  center center no-repeat',
@@ -76,27 +74,14 @@ export default function main(_) {
     _.__event.scroll.handle.push(() => {
         let openButton = $('#open-button');
 
-        if (_.__event.scroll.temScroll < _.__event.scroll.docScroll) { // 向下滚动
+        if (_.__event.scroll.temScroll < _.__event.scroll.docScroll && _.__event.scroll.homeScroll <= _.__event.scroll.docScroll && !openButton.hasClass('menu-button-scroll')) { // 向下滚动
+            openButton.addClass('menu-button-scroll');
+            openButton.text('');
+        }
 
-            if (_.__event.scroll.homeScroll <= _.__event.scroll.docScroll) { // 滚过头图
-
-                // 设置菜单按钮
-                if (!openButton.hasClass('menu-button-scroll')) {
-                    openButton.addClass('menu-button-scroll');
-                    openButton.text('');
-                }
-            }
-
-        } else { // 向上滚动
-
-            if (_.__event.scroll.homeScroll >= _.__event.scroll.docScroll) { // 滚入头图
-
-                // 设置菜单按钮
-                if (openButton.hasClass('menu-button-scroll')) {
-                    openButton.removeClass('menu-button-scroll');
-                    openButton.text('MENU');
-                }
-            }
+        if (_.__event.scroll.temScroll > _.__event.scroll.docScroll && _.__event.scroll.homeScroll >= _.__event.scroll.docScroll && openButton.hasClass('menu-button-scroll')) { // 滚入头图
+            openButton.removeClass('menu-button-scroll');
+            openButton.text('MENU');
         }
     });
 }
