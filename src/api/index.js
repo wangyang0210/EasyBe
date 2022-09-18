cc215c2023884d014ac6da80b5d3ec3234093b2c
wangyang <wangyang.0210@foxmail.com>
2022-09-18 23:13:15 +0800

    refactor: jinrishici api

--- src/api/index.js
+++ src/api/index.js
@@ -6,6 +6,7 @@
  * ----------------------------------------------
  * @describe: api接口统一封装
  */
+
 import request from '../utils/request';
 
 /**
@@ -21,7 +22,6 @@ export function getConfigInfo (baseUrl, url) {
         requestBaseUrl: baseUrl
     })
 }
-
 
 /**
  * 获取网站统计
@@ -52,5 +52,22 @@ export function getOnline (baseUrl, url) {
     })
 }
 
+/**
+ * 获取今日诗词的TOKEN
+ * @return {*}
+ */
+export function getJinrishiciToken () {
+    return request({
+        requestBaseUrl: 'https://v2.jinrishici.com/one.json?client=npm-sdk/1.0'
+    })
+}
 
-
+/**
+ * 获取今日诗词
+ * @param {string} token 今日诗词token
+ */
+export function getJinrishici (token) {
+    return request({
+        requestBaseUrl: `https://v2.jinrishici.com/one.json?client=npm-sdk/1.0&X-User-Token=${token}`
+    })
+}
