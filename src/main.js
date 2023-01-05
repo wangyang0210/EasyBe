bff89679152412fb4b8748063c6e06cd4b428033
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-01-05 20:35:00 +0800

    chore: main.js

--- src/main.js
+++ src/main.js
@@ -2,60 +2,48 @@
  * UPDATES AND DOCS AT: https://github.com/wangyang0210
  * https://www.cnblogs.com/wangyang0210/
  * @author: WangYang, wangyang.0210@foxmail.com
- * @Date 2022-08-25 15:36
+ * @Date 2022-08-25 15:26
  * ----------------------------------------------
  * @describe: 主程序文件
  */
-import _ from 'lodash/core.min';
-import config from './components/config/config';
-import status from "./components/status/status";
+import defaultConfig from './components/config/config.json5';
+import status from './components/status/status';
 import tools from './utils/tools';
 import event from "./components/event/event";
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     // 初始化
-    _.__config  = config(); // 配置信息
-    _.__status  = status(); // 页面状态信息
-    _.__tools   = tools();  // 公共处理工具
-    _.__timeIds = {};       // 定时器
-    _.__event   = {};       // 事件
+    $.__config = $.extend( true, defaultConfig,  window?.cnblogsConfig || {}); // 配置信息
+    $.__status = status; // 页面状态信息
+    $.__tools = tools;  // 公共处理工具
+    $.__timeIds = {};  // 定时器
+    $.__event = {};   // 事件
+    $.__config.info.name ||= $.__status.user;
+    $.__tools.dynamicLoadingJs($.__config.default.moment).then(r => {
+        import(/* webpackChunkName: "page-[request]" */ /* webpackPrefetch: true */ `./page/${ $.__status.pageType}`).then(module => {
+            const page = module.default;
 
+            // 前置公共处理
+            import(/* webpackChunkName: "com-before" */ /* webpackPrefetch: true */ './components/common/comBefore').then(beforeModule => {
+                const comBefore = beforeModule.default;
+                comBefore();
 
-    if (_.__config.info.name === '') _.__config.info.name = _.__status.user;
+                // 页面逻辑处理
+                page();
 
-    // 开启渲染
-    import(/* webpackChunkName: "page-[request]" */ `./page/${_.__status.pageType}`).then(module => {
-        const page = module.default;
-
-        /**
-         * 前置公共处理
-         */
-        import(/* webpackChunkName: "com-before" */ './components/common/comBefore').then(module => {
-            const comBefore = module.default;
-            comBefore(_);
-
-            /**
-             * 页面逻辑处理
-             */
-            page(_);
-
-            /**
-             * 后置公共处理
-             */
-            import(/* webpackChunkName: "com-after" */ './components/common/comAfter').then(module => {
-                const comAfter = module.default;
-                comAfter(_);
-
-                /**
-                 * 辅助处理
-                 */
-                (() => {
-                    _.__tools.setDomHomePosition(); // 文章主体位置修正
-                    event(_).handle.scroll(); // 触发滚动处理
-                    event(_).handle.resize(); // 触发窗口大小变化处理
-                })();
+                // 后置公共处理
+                import(/* webpackChunkName: "com-after" */ /* webpackPrefetch: true */ './components/common/comAfter').then(afterModule => {
+                    const comAfter = afterModule.default;
+                    comAfter();
+                    (() => {
+                        $.__tools.setDomHomePosition(); // 文章主体位置修正
+                        event.handle.scroll(); // 触发滚动处理
+                        event.handle.resize(); // 触发窗口大小变化处理
+                    })();
+                });
             });
         });
-    });
+    }).catch(e => console.error('moment.js', e));
+
 })
