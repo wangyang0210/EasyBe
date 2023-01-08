f831e1727e364870dbf5488e27b457ec40471318
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-01-08 19:20:09 +0800

    refactor: banner.js

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -2,15 +2,13 @@
  * UPDATES AND DOCS AT: https://github.com/wangyang0210
  * https://www.cnblogs.com/wangyang0210/
  * @author: WangYang, wangyang.0210@foxmail.com
- * @Date 2022-08-25 15:31
+ * @Date 2022-08-25 15:19
  * ----------------------------------------------
- * @describe: banner 处理
+ * @describe: banner背景图片处理
  */
 import bannerTemp from '../../template/banner.html';
-import homeTopBg from "../../images/webp/home_top_bg.webp";
-import netHomeTopBg from "../../images/webp/nothome_top_bg.webp";
 
-export default function main(_) {
+export default function main() {
 
     $('#sidebar_news').prepend(bannerTemp);
 
@@ -23,10 +21,10 @@ export default function main(_) {
         let topImg, bgImg, height;
 
         // 设置图片
-        if (_.__status.pageType === 'home') {
-            topImg = _.__config.banner.home.background.length > 0 ? _.__config.banner.home.background : [homeTopBg];
+        if ( $.__status.pageType === 'home') {
+            topImg = $.__config.banner.home.background.length > 0 ? $.__config.banner.home.background : ['https://images.cnblogs.com/cnblogs_com/wangyang0210/1943283/o_220917053600_wallhaven-6k3oox.webp'];
         }  else {
-            topImg = _.__config.banner.article.background.length > 0 ? _.__config.banner.article.background : [netHomeTopBg];
+            topImg = $.__config.banner.article.background.length > 0 ? $.__config.banner.article.background : [	'https://images.cnblogs.com/cnblogs_com/wangyang0210/1943283/o_220917053937_wallhaven-j5mz95.webp'];
             height = '40vh';
             $('#homeTopTitle').hide();
             $('.scroll-down').hide();
@@ -38,25 +36,25 @@ export default function main(_) {
         if (height) mainHeader.css('height', height);
 
         // banner动效
-        if (_.__config.animate.bannerImages.enable) {
+        if ( $.__config.animate.bannerImages?.enable) {
             // 开启图片自动切换
-            import(/* webpackChunkName: "banner-images" */ '../bannerImages/bannerImages').then(module => {
+            import(/* webpackChunkName: "banner-images" */ /* webpackPrefetch: true */ '../bannerImages/bannerImages').then(module => {
                 let bannerImages = module.default;
                 bannerImages(
                     'main-header',
                     topImg,
-                    _.__config.animate.bannerImages.options.itemNum,
-                    _.__config.animate.bannerImages.options.time,
-                    _.__config.animate.bannerImages.options.sort,
-                    _.__config.animate.bannerImages.options.current < 0
-                        ? _.__tools.randomNum(0, topImg.length - 1)
-                        : _.__config.animate.bannerImages.options.current
+                    $.__config.animate.bannerImages.options.itemNum,
+                    $.__config.animate.bannerImages.options.time,
+                    $.__config.animate.bannerImages.options.sort,
+                    $.__config.animate.bannerImages.options.current < 0
+                        ? $.__tools.randomNum(0, topImg.length - 1)
+                        : $.__config.animate.bannerImages.options.current
                 );
             });
         } else {
             // 随机指定一个图片
 
-            if (topImg.length > 1) bgImg = topImg[_.__tools.randomNum(0, topImg.length - 1)]
+            if (topImg.length > 1) bgImg = topImg[ $.__tools.randomNum(0, topImg.length - 1)]
             else bgImg = topImg[0] || ""
 
             mainHeader.css({
@@ -66,20 +64,20 @@ export default function main(_) {
         }
 
         // Banner文字是否可选
-        if(!_.__config.banner.text) $('.main-header-content.inner').addClass('textUnselect')
+        if(! $.__config.banner.text) $('.main-header-content.inner').addClass('textUnselect')
 
     })();
 
     // 添加事件监听
-    _.__event.scroll.handle.push(() => {
+    $.__event.scroll.handle.push(() => {
         let openButton = $('#open-button');
 
-        if (_.__event.scroll.temScroll < _.__event.scroll.docScroll && _.__event.scroll.homeScroll <= _.__event.scroll.docScroll && !openButton.hasClass('menu-button-scroll')) { // 向下滚动
+        if ( $.__event.scroll.temScroll < $.__event.scroll.docScroll && $.__event.scroll.homeScroll <= $.__event.scroll.docScroll && !openButton.hasClass('menu-button-scroll')) { // 向下滚动
             openButton.addClass('menu-button-scroll');
             openButton.text('');
         }
 
-        if (_.__event.scroll.temScroll > _.__event.scroll.docScroll && _.__event.scroll.homeScroll >= _.__event.scroll.docScroll && openButton.hasClass('menu-button-scroll')) { // 滚入头图
+        if ( $.__event.scroll.temScroll > $.__event.scroll.docScroll && $.__event.scroll.homeScroll >= $.__event.scroll.docScroll && openButton.hasClass('menu-button-scroll')) { // 滚入头图
             openButton.removeClass('menu-button-scroll');
             openButton.text('MENU');
         }
