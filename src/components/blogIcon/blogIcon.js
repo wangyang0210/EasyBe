9120524d2b422aa5366a020e4dfb0c7b26e5c477
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-01-11 19:36:20 +0800

    refactor: blogIcon

--- src/components/blogIcon/blogIcon.js
+++ src/components/blogIcon/blogIcon.js
@@ -2,18 +2,17 @@
  * UPDATES AND DOCS AT: https://github.com/wangyang0210
  * https://www.cnblogs.com/wangyang0210/
  * @author: WangYang, wangyang.0210@foxmail.com
- * @Date 2022-08-25 15:31
+ * @Date 2022-08-25 15:19
  * ----------------------------------------------
  * @describe: 网站图标处理
  */
-export default function main(_) {
+export default function main() {
     let shortcutIcon = $('link[rel="shortcut icon"]');
-    if (_.__config.info.blogIcon && shortcutIcon.length) shortcutIcon.attr('href', _.__config.info.blogIcon);
-    if (_.__config.info.blogIcon && !shortcutIcon.length) {
+    if ( $.__config.info.blogIcon && shortcutIcon.length) shortcutIcon.attr('href', $.__config.info.blogIcon);
+    if ( $.__config.info.blogIcon && !shortcutIcon.length) {
         let linkObject  = document.createElement('link');
         linkObject.rel  = "shortcut icon";
-        linkObject.href = _.__config.info.blogIcon;
+        linkObject.href = $.__config.info.blogIcon;
         document.getElementsByTagName("head")[0].appendChild(linkObject);
     }
 }
-
