7c54cdb2d58d0838557267a5d75ba204cb8f28f4
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-01-13 21:12:32 +0800

    ârefactor: comment.js

--- src/components/comment/comment.js
+++ src/components/comment/comment.js
@@ -2,13 +2,12 @@
  * UPDATES AND DOCS AT: https://github.com/wangyang0210
  * https://www.cnblogs.com/wangyang0210/
  * @author: WangYang, wangyang.0210@foxmail.com
- * @Date 2022-08-25 15:32
+ * @Date 2022-08-25 15:20
  * ----------------------------------------------
  * @describe: 评论处理
  */
-import defaultAvatarImg from './../../images/webp/default_avatar.webp';
 
-export default function main(_) {
+export default function main() {
     let setComment = () => {
         let feedbackItem = $('.feedbackItem');
         if (feedbackItem.length > 0) {
@@ -21,30 +20,34 @@ export default function main(_) {
                     idInfo = commentBody.length ? commentBody.attr('id').split('_') : undefined;
 
                 if (idInfo && idInfo.length > 0) {
-                    let id = idInfo[idInfo.length - 1], idTmp = id.toString().match(/\d/g);
+                    let id = idInfo[idInfo.length - 1],
+                        idTmp = id.toString().match(/\d/g);
+
                     if ($.isArray(idTmp)) id = idTmp.join('');
-                    let op = $('#comment_' + id + '_avatar'), patch  = op.length > 0 ? op.text().trim() : defaultAvatarImg;
+
+                    let op = $('#comment_' + id + '_avatar'), patch  = op.length > 0 ? op.text().trim() : $.__config.default.avatar;
+
                     let ac = $('#a_comment_author_' + id), ah = ac.length ? ac.attr('href') : 'javascropt:void(0);';
+
                     avatarHtml = '<div class="feedbackAvatar"><a href="' + ah + '" target="_blank"><img src="'+patch+'"/></a></div>';
                     obj.prepend(avatarHtml);
                 }
+
                 if (feedbackListSubtitle.length && feedbackListSubtitle.find('.louzhu').length) feedbackListSubtitle.addClass('feedbackListSubtitle-louzhu');
             });
             $(feedbackItem[0]).css('padding-top', '0');
             $(feedbackItem[feedbackItem.length - 1]).css('padding-bottom', '0');
-            _.__config.animate.avatar.enable && $('.feedbackAvatar').addClass('img-rounded')
+            $.__config.animate.avatar.enable && $('.feedbackAvatar').addClass('img-rounded')
         }
     }
-    _.__timeIds.commentTId = window.setInterval(() =>{
+    $.__timeIds.commentTId = window.setInterval(() =>{
         if ($('.feedbackItem').length > 0) {
             setComment();
-            _.__tools.clearIntervalTimeId(_.__timeIds.commentTId);
+            $.__tools.clearIntervalTimeId( $.__timeIds.commentTId);
         }
     },1000);
 
     $(document).ajaxSuccess(function (event, xhr, settings) {
-        if (settings.url.includes("archive")) setComment();
+        if (settings.url.includes("GetComments.aspx")) setComment();
     });
 }
-
-
