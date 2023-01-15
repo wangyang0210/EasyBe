eb1d974055cf5b2055b34148eedd6153ed7037ff
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-01-15 16:52:14 +0800

    refactor: imgbox

--- src/components/imgBox/imgBox.js
+++ src/components/imgBox/imgBox.js
@@ -2,14 +2,14 @@
  * UPDATES AND DOCS AT: https://github.com/wangyang0210
  * https://www.cnblogs.com/wangyang0210/
  * @author: WangYang, wangyang.0210@foxmail.com
- * @Date 2022-08-25 15:33
+ * @Date 2022-08-25 15:22
  * ----------------------------------------------
  * @describe: 图片灯箱处理
  */
-import "../../vendor/fancybox/jquery.fancybox";
-import "../../vendor/fancybox/jquery.fancybox.min.css";
+await $.__tools.dynamicLoadingCss($.__config.default.fancyboxcss)
+await $.__tools.dynamicLoadingJs($.__config.default.fancybox).catch(e => console.error('fancybox.js', e))
+export default function main() {
 
-export default function main(_) {
     setTimeout(() =>  {
         let imgLength   = $('#cnblogs_post_body img').length -1;
         if (!imgLength) return;
@@ -19,9 +19,13 @@ export default function main(_) {
             ,comImgList = $('.feedbackItem img')
             ,data       = [];
 
-        $.each(imgList, function (i) { data.push(imgList[i]); });
+        $.each(imgList, function (i) {
+            data.push(imgList[i]);
+        });
 
-        $.each(comImgList, function (i) { data.push(comImgList[i]); });
+        $.each(comImgList, function (i) {
+            data.push(comImgList[i]);
+        });
 
         if (cpb.length > 0 && data.length > 0) {
             $.each(data, (i) => {
