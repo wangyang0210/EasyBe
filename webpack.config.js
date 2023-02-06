bdecdc66ce61b5d9479c5af25da7cf30583e2c36
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-02-06 14:21:24 +0800

    webpack: del imgDir

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
                         { source: './easybe/simple-memory.js', destination: './easybe/simple-memory.' + randomString(8) + '.js' },
                         { source: './src/php/*.php', destination: './easybe/' },
                         { source: './src/php/*.png', destination: './easybe/' },
-                        { source: './src/images/banner/*.webp', destination: './easybe/images/' },
+                        // { source: './src/images/banner/*.webp', destination: './easybe/images/' },
                     ],
                 }
             }
