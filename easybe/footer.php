acc9b46cf7a8ea13376f07813b9461b2bf90a439
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-02-14 21:59:14 +0800

    fix: fix close comments style

---
+++ easybe/footer.php
@@ -0,0 +1,76 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+    </div><!--end: main -->
+</div><!--end: home 自定义的最大容器 -->
+
+<!--start: footer -->
+  <div id="footer">
+        <!--done-->
+        Copyright © 2022 。思索
+        <br><span id="poweredby">Powered by Typecho</span>
+
+  </div>
+<!--end: footer -->
+
+<?php $this->options->globalConfig()?>
+
+<?php $this->footer(); ?>
+
+<!--事件监听-->
+<script>
+    // 点赞
+    function agree(url, cid) {
+        $.ajax({
+            type: 'post',
+            url: url,
+            data: `agree=${cid}`,
+            async: true,
+            timeout: 30000,
+            cache: false,
+            //  请求成功的函数
+            success: function (data) {
+                $('#digg_count').text(data)
+            },
+            error: function () {
+                // 错误处理
+            },
+        })
+    };
+
+    // 关注博主
+    function follow() {
+        $('.hideRightMenu').show();
+        $('#rightDashang .rightMenuSpan').hide();
+        $('#rightGzh .rightMenuSpan').show();
+    };
+
+    // 打赏博主
+    function sponsor() {
+        $('.hideRightMenu').show();
+        $('#rightGzh .rightMenuSpan').hide();
+        $('#rightDashang .rightMenuSpan').show();
+    };
+    // 评论
+    function comments(url) {
+        $.ajax({
+            type: 'post',
+            url: url,
+            data:  $('#comment-form').serializeArray(),
+            async: true,
+            timeout: 30000,
+            cache: false,
+            success: function (data) {
+                $('#comments').html($("#comments", data).html());
+                // $('.comment-list').html($(".comment-list", data).html());
+                $('#tbCommentBody').val("");
+            },
+            error: function () {
+                alert('评论提交失败(っ °Д °;)っ');
+            },
+        })
+    };
+
+</script>
+
+</body>
+</html>
---
+++ easybe/script/gf-blink.8a58cd07.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkeasybe=self.webpackChunkeasybe||[]).push([[297],{724:function(e,s,u){u.r(s)}}]);
