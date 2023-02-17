1b025fdefe1b2e4b86d2ca1bd8e49f551228ef17
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-02-17 21:47:53 +0800

    refactor: add some config

--- src/typecho/functions.php
+++ src/typecho/functions.php
@@ -11,12 +11,12 @@ function themeConfig($form) {
     $globalConfig = new Typecho_Widget_Helper_Form_Element_Textarea('globalConfig', NULL, '<script src="//cdn.jsdelivr.net/gh/wangyang0210/EasyBe@v2.1.7/easybe/simple-memory.js" defer></script>', _t('全局配置'));
     $form->addInput($globalConfig);
 
-    $postRank = new Typecho_Widget_Helper_Form_Element_Text('text', NULL, '10', _t('阅读排行'), _t('阅读排行显示数量'));
-    $postRank->input->setAttribute('class', 'w-100');
+    $postRank = new Typecho_Widget_Helper_Form_Element_Text('postRank', NULL, '10', _t('阅读排行'), _t('阅读排行显示数量'));
+    $postRank->input->setAttribute('class', 'w-20');
     $form->addInput($postRank->addRule('isInteger', _t('请输入纯数字')));
 
-    $commentsRank = new Typecho_Widget_Helper_Form_Element_Text('text', NULL, '10', _t('评论排行'), _t('评论排行显示数量'));
-    $commentsRank->input->setAttribute('class', 'w-100');
+    $commentsRank = new Typecho_Widget_Helper_Form_Element_Text('commentsRank', NULL, '10', _t('评论排行'), _t('评论排行显示数量'));
+    $commentsRank->input->setAttribute('class', 'w-20');
     $form->addInput($commentsRank->addRule('isInteger', _t('请输入纯数字')));
 
 
@@ -32,32 +32,28 @@ function themeConfig($form) {
     $form->addInput($sidebarBlock->multiMode());
 }
 
-// TODO 重写所有SQL
-
-// 全部文章阅读量
+// 获取全部文章阅读量
 function getAllPostViews() {
     $db = Typecho_Db::get();
-    $row = $db->fetchAll('select sum(views) as views from `typecho_contents`;');
-    return $row[0]['views'];
+    if (!array_key_exists('views', $db->fetchRow($db->select()->from('table.contents')))) {
+        $db->query("ALTER TABLE `{$db->getPrefix()}contents` ADD `views` INT(10) DEFAULT 0;");
+    }
+    $row = $db->fetchAll("select sum(views) as views from `{$db->getPrefix()}contents`;");
+    echo $row[0]['views'];
 }
 
 
-// 指定文章的阅读访问量
+// 获取指定文章的阅读访问量
 function getPostView($archive) {
     $cid    = $archive->cid;
     $db     = Typecho_Db::get();
-    if (!array_key_exists('views', $db->fetchRow($db->select()->from('table.contents')))) {
-        $db->query('ALTER TABLE `' . $db->getPrefix() . 'contents` ADD `views` INT(10) DEFAULT 0;');
-        return 0;
-    }
     $row = $db->fetchRow($db->select('views')->from('table.contents')->where('cid = ?', $cid))['views'];
     if ($archive->is('single')) $db->query($db->update('table.contents')->rows(array('views' => (int) $row + 1))->where('cid = ?', $cid));
-    return $row;
+    echo $row;
 }
 
-
-// 获取文章阅读排行
-function getPostViewRank($limit = 10) {
+// 获取阅读量最多的文章
+function getPostViewRank($limit) {
     $db     = Typecho_Db::get();
     $posts = $db->fetchAll($db->select()->from('table.contents')
         ->where('type = ? AND status = ? AND password is NULL', 'post', 'publish')
@@ -66,20 +62,18 @@ function getPostViewRank($limit = 10) {
     );
 
     if ($posts) {
-        $postViewsRank = '';
         foreach ($posts as $post) {
             $result = Typecho_Widget::widget('Widget_Abstract_Contents')->push($post);
             $postTitle = htmlspecialchars($result['title']);
             $permalink = $result['permalink'];
             $postViews = $result['views'];
-            $postViewsRank .= "<li><a href='$permalink' title='$postTitle'>$postTitle($postViews)</a></li>";
+            echo "<li><a href='$permalink' title='$postTitle'>$postTitle($postViews)</a></li>";
         }
-        return $postViewsRank;
     }
 }
 
-// 获取文章评论排行
-function getPostCommentRank($limit = 10) {
+// 获取评论最多的文章
+function getPostCommentRank($limit) {
     $db     = Typecho_Db::get();
     $posts = $db->fetchAll($db->select()->from('table.contents')
         ->where('type = ? AND status = ? AND password is NULL', 'post', 'publish')
@@ -88,15 +82,13 @@ function getPostCommentRank($limit = 10) {
     );
 
     if ($posts) {
-        $postCommentRank = '';
         foreach ($posts as $post) {
             $result = Typecho_Widget::widget('Widget_Abstract_Contents')->push($post);
             $postTitle = htmlspecialchars($result['title']);
             $permalink = $result['permalink'];
             $postCommentsNum = $result['commentsNum'];
-            $postCommentRank .= "<li><a href='$permalink' title='$postTitle'>$postTitle($postCommentsNum)</a></li>";
+            echo "<li><a href='$permalink' title='$postTitle'>$postTitle($postCommentsNum)</a></li>";
         }
-        return $postCommentRank;
     }
 }
 
@@ -108,44 +100,40 @@ function getPermalinkFromCoid($coid) {
     return '<a href="#comment-'.$coid.'">@'.$row['author'].'</a>';
 }
 
-// 点赞数量
+// 获取指定文章的点赞数量
 function agreeNum($cid, &$record = NULL) {
     $db = Typecho_Db::get();
-    $callback = array(
+    $res = [
         'agree' => 0,
         'recording' => false
-    );
-
-    if (!array_key_exists('agree', $data = $db->fetchRow($db->select()->from('table.contents')->where('cid = ?', $cid)))) {
-        $db->query('ALTER TABLE `' . $db->getPrefix() . 'contents` ADD `agree` INT(10) NOT NULL DEFAULT 0;');
+    ];
+    $data = $db->fetchRow($db->select()->from('table.contents')->where('cid = ?', $cid));
+    if (!array_key_exists('agree', $data)) {
+        $db->query("ALTER TABLE `{$db->getPrefix()}contents` ADD `agree` INT(10) NOT NULL DEFAULT 0;");
     } else {
-        $callback['agree'] = $data['agree'];
+        $res['agree'] = $data['agree'];
     }
-
-    if (empty($recording = Typecho_Cookie::get('__typecho_agree_record'))) {
+    $recording = Typecho_Cookie::get('__typecho_agree_record');
+    if (empty($recording)) {
         Typecho_Cookie::set('__typecho_agree_record', '[]');
     } else {
-        $callback['recording'] = is_array($record = json_decode($recording)) && in_array($cid, $record);
+        $record = json_decode($recording);
+        $res['recording'] = is_array($record) && in_array($cid, $record);
     }
-
-    return $callback;
+    return $res;
 }
 
-// 点赞
+// 请求点赞
 function agree($cid) {
     $db = Typecho_Db::get();
-    $callback = agreeNum($cid, $record);
-
+    $res = agreeNum($cid, $record);
     if (empty($record)) {
         Typecho_Cookie::set('__typecho_agree_record', "[$cid]");
     } else {
-        if ($callback['recording']) return $callback['agree'];
+        if ($res['recording']) return $res['agree'];
         array_push($record, $cid);
         Typecho_Cookie::set('__typecho_agree_record', json_encode($record));
     }
-
-    $db->query('UPDATE `' . $db->getPrefix() . 'contents` SET `agree` = `agree` + 1 WHERE `cid` = ' . $cid . ';');
-
-    return ++$callback['agree'];
+    $db->query("UPDATE `{$db->getPrefix()}contents` SET `agree` = `agree` + 1 WHERE `cid` = {$cid};");
+    return ++$res['agree'];
 }
-
