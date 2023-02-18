686870527f408b5dd6ec51b38c4bf99b82bf7782
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-02-18 11:52:15 +0800

    add: owo adaptation easybe

--- easybe/script/owo-css.f59ac644.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkeasybe=self.webpackChunkeasybe||[]).push([[798],{6311:function(e,s,u){u.r(s)}}]);
--- src/components/comment/owo/owo.js
+++ src/components/comment/owo/owo.js
@@ -1,107 +1,118 @@
-(() => {
-    class OwO {
-        constructor(option) {
-            const defaultOption = {
-                logo: 'OwO表情',
-                container: document.getElementsByClassName('OwO')[0],
-                target: document.getElementsByTagName('textarea')[0],
-                position: 'down',
-                width: '100%',
-                maxHeight: '250px',
-                data: $.__config.comment.emoticon
-            };
-            for (let defaultKey in defaultOption) {
-                if (defaultOption.hasOwnProperty(defaultKey) && !option.hasOwnProperty(defaultKey))  option[defaultKey] = defaultOption[defaultKey];
+/**
+ * UPDATES AND DOCS AT: https://github.com/wangyang0210
+ * https://www.cnblogs.com/wangyang0210/
+ * @author: WangYang, wangyang.0210@foxmail.com
+ * @Date 2023-02-18 11:37
+ * ----------------------------------------------
+ * @describe: owo.js
+ */
+
+
+class OwO {
+    constructor(options) {
+        const defaultOption = {
+            logo: 'OwO表情',
+            container: document.getElementsByClassName('OwO')[0],
+            target: document.getElementsByTagName('textarea')[0],
+            position: 'down',
+            width: '100%',
+            maxHeight: '250px',
+            data: $.__config.comment.emoticon
+        };
+        for (let defaultKey in defaultOption) {
+            if (defaultOption.hasOwnProperty(defaultKey) && !options.hasOwnProperty(defaultKey))  options[defaultKey] = defaultOption[defaultKey];
+        }
+        this.container = options.container;
+        this.target = options.target;
+        if (options.position === 'up')  this.container.classList.add('OwO-up');
+    }
+
+    init(options) {
+        this.area = options.target;
+        this.packages = Object.keys(this.data);
+
+        // fill in HTML
+        let html = `
+            <div class="OwO-logo"><span>${options.logo}</span></div>
+            <div class="OwO-body" style="width: ${options.width}">`;
+
+        for (let i = 0; i < this.packages.length; i++) {
+            html += `<ul class="OwO-items OwO-items-${this.data[this.packages[i]].type}" style="max-height: ${parseInt(options.maxHeight) - 53 + 'px'};">`;
+            let opackage = this.data[this.packages[i]].container;
+            for (let i = 0; i < opackage.length; i++) {
+                html += `<li class="OwO-item" title="${opackage[i].text}">${opackage[i].icon}</li>`;
             }
-            this.container = option.container;
-            this.target = option.target;
-            if (option.position === 'up')  this.container.classList.add('OwO-up');
+            html += `</ul>`;
         }
 
-        init(option) {
-            this.area = option.target;
-            this.packages = Object.keys(this.data);
-
-            // fill in HTML
-            let html = `
-            <div class="OwO-logo"><span>${option.logo}</span></div>
-            <div class="OwO-body" style="width: ${option.width}">`;
-            
-            for (let i = 0; i < this.packages.length; i++) {
-                html += `<ul class="OwO-items OwO-items-${this.data[this.packages[i]].type}" style="max-height: ${parseInt(option.maxHeight) - 53 + 'px'};">`;
-                let opackage = this.data[this.packages[i]].container;
-                for (let i = 0; i < opackage.length; i++) {
-                    html += `<li class="OwO-item" title="${opackage[i].text}">${opackage[i].icon}</li>`;
-                }
-                html += `</ul>`;
-            }
-            
-            html += `
+        html += `
                 <div class="OwO-bar">
                     <ul class="OwO-packages">`;
-                    for (let i = 0; i < this.packages.length; i++) {
-                        html += `<li><span>${this.packages[i]}</span></li>`
-                    }
-
-            html += `
+        for (let i = 0; i < this.packages.length; i++) {
+            html += `<li><span>${this.packages[i]}</span></li>`
+        }
+        html += `
                     </ul>
                 </div>
             </div>
             `;
-            this.container.innerHTML = html;
+        this.container.innerHTML = html;
 
-            // bind event
-            this.logo = this.container.getElementsByClassName('OwO-logo')[0];
-            this.logo.addEventListener('click', () => { this.toggle(); });
+        // bind event
+        this.logo = this.container.getElementsByClassName('OwO-logo')[0];
+        this.logo.addEventListener('click', () => { this.toggle(); });
 
-            this.container.getElementsByClassName('OwO-body')[0].addEventListener('click', (e)=> {
-                let target = null;
-                if (e.target.classList.contains('OwO-item')) {
-                    target = e.target;
-                }else if (e.target.parentNode.classList.contains('OwO-item')) {
-                    target = e.target.parentNode;
-                }
-                if (target) {
-                    const cursorPos = this.area.selectionEnd;
-                    let areaValue = this.area.value;
-                    this.area.value = areaValue.slice(0, cursorPos) + target.innerHTML + areaValue.slice(cursorPos);
-                    this.area.focus();
-                    this.toggle();
-                }
-            });
-
-            this.packagesEle = this.container.getElementsByClassName('OwO-packages')[0];
-            for (let i = 0; i < this.packagesEle.children.length; i++) {
-                ((index) =>{
-                    this.packagesEle.children[i].addEventListener('click', () => {
-                        this.tab(index);
-                    });
-                })(i);
+        this.container.getElementsByClassName('OwO-body')[0].addEventListener('click', (e)=> {
+            let target = null;
+            if (e.target.classList.contains('OwO-item')) {
+                target = e.target;
+            }else if (e.target.parentNode.classList.contains('OwO-item')) {
+                target = e.target.parentNode;
             }
-            this.tab(0);
-        }
-
-        toggle() {
-            if (this.container.classList.contains('OwO-open')) {
-                this.container.classList.remove('OwO-open');
-            } else {
-                this.container.classList.add('OwO-open');
+            if (target) {
+                const cursorPos = this.area.selectionEnd;
+                let areaValue = this.area.value;
+                this.area.value = areaValue.slice(0, cursorPos) + target.innerHTML + areaValue.slice(cursorPos);
+                this.area.focus();
+                this.toggle();
             }
+        });
+
+        this.packagesEle = this.container.getElementsByClassName('OwO-packages')[0];
+        for (let i = 0; i < this.packagesEle.children.length; i++) {
+            ((index) =>{
+                this.packagesEle.children[i].addEventListener('click', () => {
+                    this.tab(index);
+                });
+            })(i);
         }
+        this.tab(0);
+    }
 
-        tab(index) {
-            const itemsShow = this.container.getElementsByClassName('OwO-items-show')[0];
-            if (itemsShow) itemsShow.classList.remove('OwO-items-show');
-            this.container.getElementsByClassName('OwO-items')[index].classList.add('OwO-items-show');
-
-            const packageActive = this.container.getElementsByClassName('OwO-package-active')[0];
-            if (packageActive)  packageActive.classList.remove('OwO-package-active');
-            this.packagesEle.getElementsByTagName('li')[index].classList.add('OwO-package-active');
+    toggle() {
+        if (this.container.classList.contains('OwO-open')) {
+            this.container.classList.remove('OwO-open');
+        } else {
+            this.container.classList.add('OwO-open');
         }
     }
-    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-        module.exports = OwO;
-    } else {
-        window.OwO = OwO;
+
+    tab(index) {
+        const itemsShow = this.container.getElementsByClassName('OwO-items-show')[0];
+        if (itemsShow) itemsShow.classList.remove('OwO-items-show');
+        this.container.getElementsByClassName('OwO-items')[index].classList.add('OwO-items-show');
+
+        const packageActive = this.container.getElementsByClassName('OwO-package-active')[0];
+        if (packageActive)  packageActive.classList.remove('OwO-package-active');
+        this.packagesEle.getElementsByTagName('li')[index].classList.add('OwO-package-active');
     }
-})();
+}
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = OwO;
+} else {
+    window.OwO = OwO;
+}
+
+export default function main(options) {
+   new OwO(options)
+}
