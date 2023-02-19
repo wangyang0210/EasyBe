642091dfc9aac1f129d50129beffca416e44a8e4
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-02-19 14:39:54 +0800

    add: commentTyping

--- src/components/comment/commentTyping/commentTyping.js
+++ src/components/comment/commentTyping/commentTyping.js
@@ -1,2 +1,138 @@
-(function webpackUniversalModuleDefinition(a,b){if(typeof exports==="object"&&typeof module==="object"){module.exports=b()}else{if(typeof define==="function"&&define.amd){define([],b)}else{if(typeof exports==="object"){exports["POWERMODE"]=b()}else{a["POWERMODE"]=b()}}}})(this,function(){return(function(a){var b={};function c(e){if(b[e]){return b[e].exports}var d=b[e]={exports:{},id:e,loaded:false};a[e].call(d.exports,d,d.exports,c);d.loaded=true;return d.exports}c.m=a;c.c=b;c.p="";return c(0)})([function(c,g,b){var d=document.createElement("canvas");d.width=window.innerWidth;d.height=window.innerHeight;d.style.cssText="position:fixed;top:0;left:0;pointer-events:none;z-index:999999";window.addEventListener("resize",function(){d.width=window.innerWidth;d.height=window.innerHeight});document.body.appendChild(d);var a=d.getContext("2d");var n=[];var j=0;var k=120;var f=k;var p=false;o.shake=true;function l(r,q){return Math.random()*(q-r)+r}function m(r){if(o.colorful){var q=l(0,360);return"hsla("+l(q-10,q+10)+", 100%, "+l(50,80)+"%, "+1+")"}else{return window.getComputedStyle(r).color}}function e(){var t=document.activeElement;var v;if(t.tagName==="TEXTAREA"||(t.tagName==="INPUT"&&t.getAttribute("type")==="text")){var u=b(1)(t,t.selectionStart);v=t.getBoundingClientRect();return{x:u.left+v.left,y:u.top+v.top,color:m(t)}}var s=window.getSelection();if(s.rangeCount){var q=s.getRangeAt(0);var r=q.startContainer;if(r.nodeType===document.TEXT_NODE){r=r.parentNode}v=q.getBoundingClientRect();return{x:v.left,y:v.top,color:m(r)}}return{x:0,y:0,color:"transparent"}}function h(q,s,r){return{x:q,y:s,alpha:1,color:r,velocity:{x:-1+Math.random()*2,y:-3.5+Math.random()*2}}}function o(){var t=e();var s=5+Math.round(Math.random()*10);while(s--){n[j]=h(t.x,t.y,t.color);j=(j+1)%500}f=k;if(!p){requestAnimationFrame(i)}if(o.shake){var r=1+2*Math.random();var q=r*(Math.random()>0.5?-1:1);var u=r*(Math.random()>0.5?-1:1);document.body.style.marginLeft=q+"px";document.body.style.marginTop=u+"px";setTimeout(function(){document.body.style.marginLeft="";document.body.style.marginTop=""},75)}}o.colorful=false;function i(){if(f>0){requestAnimationFrame(i);f--;p=true}else{p=false}a.clearRect(0,0,d.width,d.height);for(var q=0;q<n.length;++q){var r=n[q];if(r.alpha<=0.1){continue}r.velocity.y+=0.075;r.x+=r.velocity.x;r.y+=r.velocity.y;r.alpha*=0.96;a.globalAlpha=r.alpha;a.fillStyle=r.color;a.fillRect(Math.round(r.x-1.5),Math.round(r.y-1.5),3,3)}}requestAnimationFrame(i);c.exports=o},function(b,a){(function(){var d=["direction","boxSizing","width","height","overflowX","overflowY","borderTopWidth","borderRightWidth","borderBottomWidth","borderLeftWidth","borderStyle","paddingTop","paddingRight","paddingBottom","paddingLeft","fontStyle","fontVariant","fontWeight","fontStretch","fontSize","fontSizeAdjust","lineHeight","fontFamily","textAlign","textTransform","textIndent","textDecoration","letterSpacing","wordSpacing","tabSize","MozTabSize"];var e=window.mozInnerScreenX!=null;function c(k,l,o){var h=o&&o.debug||false;if(h){var i=document.querySelector("#input-textarea-caret-position-mirror-div");if(i){i.parentNode.removeChild(i)}}var f=document.createElement("div");f.id="input-textarea-caret-position-mirror-div";document.body.appendChild(f);var g=f.style;var j=window.getComputedStyle?getComputedStyle(k):k.currentStyle;g.whiteSpace="pre-wrap";if(k.nodeName!=="INPUT"){g.wordWrap="break-word"}g.position="absolute";if(!h){g.visibility="hidden"}d.forEach(function(p){g[p]=j[p]});if(e){if(k.scrollHeight>parseInt(j.height)){g.overflowY="scroll"}}else{g.overflow="hidden"}f.textContent=k.value.substring(0,l);if(k.nodeName==="INPUT"){f.textContent=f.textContent.replace(/\s/g,"\u00a0")}var n=document.createElement("span");n.textContent=k.value.substring(l)||".";f.appendChild(n);var m={top:n.offsetTop+parseInt(j["borderTopWidth"]),left:n.offsetLeft+parseInt(j["borderLeftWidth"])};if(h){n.style.backgroundColor="#aaa"}else{document.body.removeChild(f)}return m}if(typeof b!="undefined"&&typeof b.exports!="undefined"){b.exports=c}else{window.getCaretCoordinates=c}}())}])});
-POWERMODE.colorful=true;POWERMODE.shake=false;document.body.addEventListener("input",POWERMODE);
+/**
+ * UPDATES AND DOCS AT: https://github.com/wangyang0210
+ * https://www.cnblogs.com/wangyang0210/
+ * @author: https://github.com/disjukr/activate-power-mode
+ * @Date 2023-02-19 12:54
+ * ----------------------------------------------
+ * @describe: POWERMODE
+ */
+
+let canvas = document.createElement('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.cssText = 'position:fixed;top:0;left:0;pointer-events:none;z-index:999999';
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+document.body.appendChild(canvas);
+let context = canvas.getContext('2d');
+let particles = [];
+let particlePointer = 0;
+let rendering = false;
+
+POWERMODE.shake = true;
+
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function getColor(el) {
+    if (POWERMODE.colorful) {
+        let u = getRandom(0, 360);
+        return 'hsla(' + getRandom(u - 10, u + 10) + ', 100%, ' + getRandom(50, 80) + '%, ' + 1 + ')';
+    } else {
+        return window.getComputedStyle(el).color;
+    }
+}
+
+function getCaret() {
+    let el = document.activeElement;
+    let bcr;
+    if (el.tagName === 'TEXTAREA' ||
+        (el.tagName === 'INPUT' && el.getAttribute('type') === 'text')) {
+        let offset = require('./textareaCaretPosition')(el, el.selectionEnd);
+        bcr = el.getBoundingClientRect();
+        return {
+            x: offset.left + bcr.left,
+            y: offset.top + bcr.top,
+            color: getColor(el)
+        };
+    }
+    let selection = window.getSelection();
+    if (selection.rangeCount) {
+        let range = selection.getRangeAt(0);
+        let startNode = range.startContainer;
+        if (startNode.nodeType === document.TEXT_NODE) {
+            startNode = startNode.parentNode;
+        }
+        bcr = range.getBoundingClientRect();
+        return {
+            x: bcr.left,
+            y: bcr.top,
+            color: getColor(startNode)
+        };
+    }
+    return { x: 0, y: 0, color: 'transparent' };
+}
+
+function createParticle(x, y, color) {
+    return {
+        x: x,
+        y: y,
+        alpha: 1,
+        color: color,
+        velocity: {
+            x: -1 + Math.random() * 2,
+            y: -3.5 + Math.random() * 2
+        }
+    };
+}
+
+function POWERMODE() {
+    { // spawn particles
+        let caret = getCaret();
+        let numParticles = 5 + Math.round(Math.random() * 10);
+        while (numParticles--) {
+            particles[particlePointer] = createParticle(caret.x, caret.y, caret.color);
+            particlePointer = (particlePointer + 1) % 500;
+        }
+    }
+    { // shake screen
+        if (POWERMODE.shake) {
+            let intensity = 1 + 2 * Math.random();
+            let x = intensity * (Math.random() > 0.5 ? -1 : 1);
+            let y = intensity * (Math.random() > 0.5 ? -1 : 1);
+            document.body.style.marginLeft = x + 'px';
+            document.body.style.marginTop = y + 'px';
+            setTimeout(function() {
+                document.body.style.marginLeft = '';
+                document.body.style.marginTop = '';
+            }, 75);
+        }
+    }
+    if(!rendering){
+        requestAnimationFrame(loop);
+    }
+};
+POWERMODE.colorful = false;
+
+function loop() {
+    rendering = true;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    let rendered = false;
+    let rect = canvas.getBoundingClientRect();
+    for (let i = 0; i < particles.length; ++i) {
+        let particle = particles[i];
+        if (particle.alpha <= 0.1) continue;
+        particle.velocity.y += 0.075;
+        particle.x += particle.velocity.x;
+        particle.y += particle.velocity.y;
+        particle.alpha *= 0.96;
+        context.globalAlpha = particle.alpha;
+        context.fillStyle = particle.color;
+        context.fillRect(
+            Math.round(particle.x - 1.5) - rect.left,
+            Math.round(particle.y - 1.5) - rect.top,
+            3, 3
+        );
+        rendered = true;
+    }
+    if(rendered){
+        requestAnimationFrame(loop);
+    }else{
+        rendering = false;
+    }
+}
+
+module.exports = POWERMODE;
