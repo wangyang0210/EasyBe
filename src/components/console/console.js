457fe6796b5100b3b02006e19dcb77c69bcc32c4
wangyang.0210@foxmail.com <wangyang.0210@foxmail.com>
2023-04-02 04:30:08 +0800

    chore: add current version console

--- src/components/console/console.js
+++ src/components/console/console.js
@@ -8,16 +8,33 @@
  */
 
 export default function main() {
-
     // 输出默认版权信息
-    let github = ["\n %c %c %c EasyBe-GitHub %c  %c https://github.com/wangyang0210/EasyBe %c \n\n", "background: #fadfa3; padding:5px 0;", "background: #fadfa3; padding:5px 0;", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;", "background: #FCEDC9; color:#030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;"];
-    window.console.log.apply(console, github);
+    let github = [
+            '\n %c %c %c EasyBe GitHub %c  %c https://github.com/wangyang0210/EasyBe %c \n\n',
+            'background: #fadfa3; padding:5px 0;',
+            'background: #fadfa3; padding:5px 0;',
+            'color: #fadfa3; background: #030307; padding:5px 0;',
+            'background: #fadfa3; padding:5px 0;',
+            'background: #FCEDC9; color:#030307; padding:5px 0;',
+            'background: #fadfa3; padding:5px 0;',
+        ],
+        version = [
+            `\n %c %c %c EasyBe Version %c  %c ${$.__config.default.version} %c \n\n`,
+            'background: #fadfa3; padding:5px 0;',
+            'background: #fadfa3; padding:5px 0;',
+            'color: #fadfa3; background: #030307; padding:5px 0;',
+            'background: #fadfa3; padding:5px 0;',
+            'background: #FCEDC9; color:#030307; padding:5px 0;',
+            'background: #fadfa3; padding:5px 0;',
+        ]
+    window.console.log.apply(console, github)
+    window.console.log.apply(console, version)
 
     // 输出用户配置信息
-    if ( $.__config.consoleList.length) {
-        $.each( $.__config.consoleList, function (i) {
-            let fl = $.__config.consoleList[i];
-            console.log('\n' + ' %c '+(fl[0])+' %c '+(fl[1])+' ' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; color:#000;padding:5px 0;');
-        });
+    if ($.__config.consoleList.length) {
+        $.each($.__config.consoleList, function (i) {
+            let fl = $.__config.consoleList[i]
+            console.log('\n' + ' %c ' + fl[0] + ' %c ' + fl[1] + ' ' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; color:#000;padding:5px 0;')
+        })
     }
 }
